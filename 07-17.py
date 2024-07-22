0756b185caf76f3cb28621f7e6b37e012c05ff6e
Jiwon Choi <bereno6@naver.com>
2024-07-22 19:31:32 +0900

    delete

--- 07-17.py
+++ 07-17.py
@@ -104,30 +104,7 @@ def up(e,b,c):
 up("min_wage_adj","min_wage_adj_up",main_frame) # 실질 최저시급 상승률 추가
 up("min_wage","min_wage_up",main_frame) # 최저시급 상승률 추가
 up("fixed_cpi","infla_up",main_frame)   #인플레율 추가 
-'''# 실질 최저시급 상승률 추가
-k=1
-main_frame["min_wage_adj_up"]=main_frame["min_wage"][0]
-rename=main_frame["min_wage_adj"] #너무길어서 rename으로 바꿈
-while k<13:
-    main_frame.loc[k,"min_wage_adj_up"]=((rename[k] - rename[k-1])/rename[k-1]) * 100
-    k =k +1
-main_frame.loc[0,"min_wage_adj_up"] =0
-# 최저시급 상승률 추가
-k=1
-main_frame["min_wage_up"]=main_frame["min_wage"][0]
-rename=main_frame["min_wage"] #너무길어서 rename으로 바꿈
-while k<13:
-    main_frame.loc[k,"min_wage_up"]=((rename[k] - rename[k-1])/rename[k-1]) * 100
-    k =k +1
-main_frame.loc[0,"min_wage_up"] =0
-#인플레율 추가
-k=1
-main_frame["infla_up"]=main_frame["min_wage"][0]
-rename=main_frame["fixed_cpi"] #너무길어서 rename으로 바꿈
-while k<13:
-    main_frame.loc[k,"infla_up"]=((rename[k] - rename[k-1])/rename[k-1]) * 100
-    k =k +1
-main_frame.loc[0,"infla_up"] =0
+
 '''
 
 
